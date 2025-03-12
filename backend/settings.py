b61607c0e5bd82633bd250bfe81ba4d981020ac7
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-03-12 03:53:17 +0300

    Fix build error

--- backend/settings.py
+++ backend/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,12 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = BASE_DIR / 'templates'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'userauths.User'
@@ -259,3 +259,6 @@ SIMPLE_JWT = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Use whitenoise storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@
 set -o errexit
 
 # Create necessary directories
-# mkdir -p staticfiles
-# mkdir -p media
+mkdir -p staticfiles
+mkdir -p media
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input --clear
-
+python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py makemigrations
