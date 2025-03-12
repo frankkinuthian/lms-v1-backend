e79e993507773d22d70836b58a23ce2a4ad1cd57
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-03-12 03:47:56 +0300

    Fix build errors attempt 2

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-# python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
 
 
 # Run migrations
